daf2f453a42eaf80f2c5e8acb0089462b16ee37f
Jan <jszewczak@sigma.ug.edu.pl>
2012-11-01 12:32:07 +0100

    - # - Dodanie projektu, dodanie doctrine 1, stworzenie podstaw do layoutu, dodanie "testu" na to czy dziala doctrine, dodanie doctrine cli, i inne

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function testDoctrineAction()
+    {
+        $testTable = Application_Model_TestTable::getInstance();
+        $test = $testTable->findAll();
+        $this->view->test = $test;
+    }
+}
+
