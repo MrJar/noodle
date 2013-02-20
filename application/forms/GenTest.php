4359ed17187c08d14779a1b3fa94f3490ba1616f
Michal <Michal@Michalkomp>
2013-02-20 20:32:49 +0100

    - # -  poprawione drobne błędy językowe i zmiana opisu generowania losowego testu

--- application/forms/GenTest.php
+++ application/forms/GenTest.php
@@ -3,25 +3,8 @@
 class Application_Form_GenTest extends Zend_Form {
 
 
-   /* 
-    public function __construct($options = null) {
-        parent::__construct($options);
-        
-        if (isset($options['przedmiot'])) {
-
-            self::$przedmiot = (int)$options['przedmiot'];
-            var_dump(self::$przedmiot);
-        }
-        if (isset($options['ileZadan'])) {
-            self::$ileZadan = (int)$options['ileZadan'];
-        }
-        
-        
-    }
-
-*/
     public function init() {
-        //$this->setAction('/tests/add')->setMethod('post');
+ 
         
         $this->setAttrib('id', 'genTest');
 
@@ -58,13 +41,12 @@ class Application_Form_GenTest extends Zend_Form {
                             'multiOptions' => $przedAdd,
                             'required' => true
                 )));
-        //$przedmiot = $this->getElement('Przedmioty_idPrzedmioty')->getValue();
+    
         
         if ($this->getAttrib('przedmiot') > 0) {
             $countZadania =  $zadania_mdl->getZadania(array('Przedmioty_idPrzedmioty = ?',$przedmiot), TRUE);
         }
-//        var_dump($countZadania);
-      //die(); 
+ 
         if (isset($countZadania)) {
             for($i=0;$i<=$countZadania;$i++) {
                 $ile[$i]=$i;
@@ -80,25 +62,9 @@ class Application_Form_GenTest extends Zend_Form {
 
             if ($this->getAttrib('przedmiot') > 0) {
                 $this->addElement($iloscElement);
-                //$ilosc = (int)$this->getElement('ilosc')->getValue();
-            }
 
-            //$przedmiot = (int)$this->getElement('Przedmioty_idPrzedmioty')->getValue();
-            /*if ($this->getAttrib('ileZadan') > 0) {
-                $zadania = $zadania_mdl->getRandom($this->getAttrib('przedmiot'),$this->getAttrib('ileZadan') );
-                foreach ($zadania as $zadanie) {
-                    
-                    $this->addElement(new Zend_Form_Element_Checkbox($zadanie['idZadania'], 
-                            array(
-                                'label' => $zadanie['tresc'],
-                                'belongsTo' => 'zadania'
-                            )));
-                }
-            }*/
+            }
         }
-        //die();
-        
-        
         
         
         $submit = new Zend_Form_Element_Submit('submit');
